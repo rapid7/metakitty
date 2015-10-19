c4ecd87d0a6031ed227391652b6bcf9041a65e18
Brent Cook <bcook@rapid7.com>
2015-10-19 07:45:19 -0500

    we no longer need to commit stats

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -41,7 +41,6 @@ echo [*] Generating community stats:
   mkdir -p $TOPLEVEL/metasploit-resource-portal/build/assets/
   cp $TOPLEVEL/stats/*.html $TOPLEVEL/metasploit-resource-portal/build
   cp -a $TOPLEVEL/stats/assets/* $TOPLEVEL/metasploit-resource-portal/build/assets/
-  git commit -m "Updated stats for `date`" .
 echo [*] --------------------------
 
 git diff
